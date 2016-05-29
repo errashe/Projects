1d59626a5b8f328e47272cf3df2769386d20bb27
e4stw00d <e4stw00d@icloud.com>
2016-05-30 01:47:51 +0500

    30.05.2016 01:47

--- experiments/meteor_game/client/main.js
+++ experiments/meteor_game/client/main.js
@@ -3,20 +3,46 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
+var canvas, ctx, player;
+Meteor.subscribe("players");
+
+Template.game.events({
+	'click #createNew': function(e) {
+		player = ""+Date.now();
+		document.querySelector("input").value = player;
+		Meteor.subscribe("players", player);
+		Meteor.call("addPlayer", player);
+	},
+	'change input': function(e) {
+		player = e.target.value;
+		Meteor.subscribe("players", player);
+	},
+	'click #c': function(e) {
+		console.log("qwe");
+		Meteor.call("move", player, 10);
+	}
 });
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
+Template.game.helpers({
+	player: function() {
+		return player;
+	}
 });
 
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
-});
+function game() {
+	var pl = Players.find({name: player}).fetch()[0];
+	if (pl != undefined) {
+		ctx.clearRect(0, 0, 500, 500);
+		ctx.strokeRect(pl.x, pl.y, 10, 10);
+	}
+
+}
+
+Template.game.rendered = function() {
+	canvas = document.querySelector("#c");
+	ctx = canvas.getContext('2d');
+
+	setInterval(game, 1000/60);
+
+}
+
--- experiments/meteor_game/server/main.js
+++ experiments/meteor_game/server/main.js
@@ -1,5 +1,16 @@
 import { Meteor } from 'meteor/meteor';
 
+Meteor.methods({
+	'addPlayer': function(name) {
+		Players.insert({name: name, x:0, y:0});
+	},
+	'move': function(name, pos) {
+		Players.update({name: name}, {$inc: {x: pos}})
+	}
+});
+
 Meteor.startup(() => {
-  // code to run on server at startup
+	Meteor.publish("players", function(name) {
+		return Players.find({name: name});
+	});
 });
