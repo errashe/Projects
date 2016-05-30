7cf47d13f8295ec3d971e804ccbbfb953ddff97d
e4stw00d <e4stw00d@icloud.com>
2016-05-31 01:36:43 +0500

    31.05.2016 01:36

--- experiments/meteor_stream/client/main.js
+++ experiments/meteor_stream/client/main.js
@@ -1,11 +1,15 @@
-import { Meteor } from "meteor/meteor"
-
 import "/libs/main.js"
 
 sendMessage = function(message) {
 	Streamer.emit('message', message);
 	console.log('me: ' + message);
 };
+
+startMadness = function() {
+	setInterval(function() {
+		sendMessage("OMG");
+	}, 1000/60);
+}
 
 Streamer.on('message', function(message) {
 	console.log('user: ' + message);
