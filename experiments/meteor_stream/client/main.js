c1202382ade73b27ac60fd07b7caa0fdb8f53172
e4stw00d <e4stw00d@icloud.com>
2016-05-31 12:46:01 +0500

    31.05.2016 12:46

--- experiments/meteor_stream/client/main.js
+++ experiments/meteor_stream/client/main.js
@@ -1,21 +1,36 @@
 var keyState = {};
 var users = {};
-var name;
+var name, canvas, ctx;
 
-sendMessage = function(message) {
-	Streamer.emit('message', message);
-	console.log('me: ' + message);
+sendMessage = function(who, message) {
+	Streamer.emit('message', who, message);
 };
 
-startMadness = function() {
-	setInterval(function() {
-		sendMessage("OMG");
-	}, 1000/60);
+Streamer.on('message', function(who, message) {
+	users[who] = message;
+});
+
+debug = function() {
+	return users;
 }
 
-Streamer.on('message', function(message) {
+function gameLoop() {
+	ctx.clearRect(0, 0, 500, 500);
+	if (keyState[37]) {
+		users[name].x -= 5
+		sendMessage(name, {x: users[name].x-5, y: users[name].y});
+	}
+	if (keyState[39]) {
+		users[name].x += 5
+		sendMessage(name, {x: users[name].x+5, y: users[name].y});
+	}
 
-});
+	for(var id in users) {
+		var user = users[id];
+		ctx.fillRect(user.x, user.y, 10, 10);
+	}
+}
+
 
 window.addEventListener('keydown',function(e){
 	keyState[e.keyCode || e.which] = true;
@@ -26,8 +41,10 @@ window.addEventListener('keyup',function(e){
 
 Template.hello.rendered = function() {
 	name = Date.now();
-	var canvas = document.querySelector("#game");
-	var ctx = canvas.getContext("2d");
+	users[name] = {x: 0, y: 0};
+	canvas = document.querySelector("#game");
+	ctx = canvas.getContext("2d");
 
-	ctx.fillRect(10, 10, 10, 10);
+	// ctx.fillRect(10, 10, 10, 10);
+	setInterval(gameLoop, 1000/60);
 }
