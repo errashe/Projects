f9723630326c6f46218dd820b7fdae9f9db2524d
e4stw00d <e4stw00d@icloud.com>
2016-06-05 22:28:01 +0500

    05.06.2016 22:28

--- experiments/ticker/client/main.js
+++ experiments/ticker/client/main.js
@@ -1,5 +1,22 @@
-Template.hello.events({
+Template.parse.rendered = function() {
+	Meteor.subscribe("matches");
+}
+
+Template.parse.helpers({
+	"matches": function() {
+		return Matches.find({}, {sort: { time: -1 }, limit: 10});
+	},
+	"color": function(stat) {
+		if(stat == "Победа") {
+			return "green";
+		} else {
+			return "red";
+		}
+	}
+});
+
+Template.parse.events({
 	'click button'(event, instance) {
-		Meteor.call("setTicker");
+		Meteor.call("parse");
 	},
 });
--- experiments/ticker/server/main.js
+++ experiments/ticker/server/main.js
@@ -1,25 +1,43 @@
-import { Meteor } from 'meteor/meteor';
+import cheerio from "cheerio"
+import request from "request"
 
-var int = null;
+function parse() {
+	var options = {
+		url: 'http://ru.dotabuff.com/players/92413647',
+		headers: {
+			"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36"
+		}
+	};
+
+	request(options, Meteor.bindEnvironment(function(err, resp, body){
+		$ = cheerio.load(body);
+		var rows = $("div.r-table div.r-row[data-link-to*='/matches/']"); // TODO: create a normal css selector
+
+		rows.each(function(i, e) {
+			var match = $(e).attr("data-link-to").split('/')[2];
+			var hero = $(e).find("a:nth-child(2)").text();
+			var stat = $(e).find("a:nth-child(1)").text();
+			var time = $(e).find("time").attr("datetime");
+
+			Matches.upsert({match: match}, { $setOnInsert: {
+				match: match,
+				hero: hero,
+				stat: stat,
+				time: time
+			}});
+		});
+	}));
+}
 
 Meteor.methods({
-	'setTicker': function() {
-		if (int == null) {
-			int = setInterval(function() {
-				console.log("TICKER");
-			}, 1000);
-		} else {
-			clearInterval(int);
-			int = null;
-		}
-	}
+	"parse": parse
 });
 
-var i = 0;
-
 Meteor.startup(() => {
-	setInterval(function() {
-		console.log(`Hello, world!${i}`);
-		i++;
-	}, 5000)
+	parse();
+	setInterval(Meteor.bindEnvironment(parse), 1000*60);
+
+	Meteor.publish("matches", function() {
+		return Matches.find({});
+	});
 });
