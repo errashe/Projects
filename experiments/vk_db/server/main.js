db3039f4049f41b471559569ad1bea92a0f4cb9b
e4stw00d <e4stw00d@icloud.com>
2016-06-06 18:22:51 +0500

    06.06.2016 18:22

--- experiments/vk_db/server/main.js
+++ experiments/vk_db/server/main.js
@@ -1,5 +1,8 @@
+import r from 'request'
 var token = "18125f3dea048c0dfc3b75b906691ee20a603e56ba0cdaaefcdca43733602d1e96b07c7a199e3077a175f";
 
 Meteor.startup(() => {
-
+	r.get("http://yandex.ru/", function(e, r) {
+		console.log(r.body);
+	});
 });
