bae9a726c55f89f28340481f5275b28f00c15593
e4stw00d <e4stw00d@icloud.com>
2016-06-19 13:08:21 +0500

    19.06.2016 13:08

---
+++ experiments/tcpsrv/functions.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"strings"
+)
+
+func fillFunctions() map[string]interface{} {
+	functions := make(map[string]interface{})
+	functions["test"] = func(conn net.Conn, params []string) {
+		conn.Write([]byte(fmt.Sprintf("Test message with params! %s\n", strings.Join(params, "|"))))
+	}
+	return functions
+}
--- experiments/tcpsrv/main.go
+++ experiments/tcpsrv/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
 
 const (
@@ -12,39 +13,42 @@ const (
 	CONN_TYPE = "tcp"
 )
 
+var functions map[string]interface{}
+
 func main() {
-	// Listen for incoming connections.
+	functions = fillFunctions()
+
 	l, err := net.Listen(CONN_TYPE, CONN_HOST+":"+CONN_PORT)
 	if err != nil {
 		fmt.Println("Error listening:", err.Error())
 		os.Exit(1)
 	}
-	// Close the listener when the application closes.
+
 	defer l.Close()
 	fmt.Println("Listening on " + CONN_HOST + ":" + CONN_PORT)
 	for {
-		// Listen for an incoming connection.
 		conn, err := l.Accept()
 		if err != nil {
 			fmt.Println("Error accepting: ", err.Error())
 			os.Exit(1)
 		}
-		// Handle connections in a new goroutine.
 		go handleRequest(conn)
 	}
 }
 
 func handleRequest(conn net.Conn) {
-	// Make a buffer to hold incoming data.
 	buf := make([]byte, 1024*10)
-	// Read the incoming connection into the buffer.
+
 	l, err := conn.Read(buf)
 	if err != nil {
 		fmt.Println("Error reading:", err.Error())
 	}
-	fmt.Println(string(buf[:l-1]))
-	// Send a response back to person contacting us.
-	conn.Write([]byte("Message received."))
-	// Close the connection when you're done with it.
+
+	req := string(buf[:l-1])
+	reqA := strings.Split(req, "|")
+
+	if functions[reqA[0]] != nil {
+		functions[reqA[0]].(func(net.Conn, []string))(conn, reqA[1:])
+	}
 	conn.Close()
 }
