c78909df8adc74f41850ea7ce4ca112a3732bef7
e4stw00d <e4stw00d@icloud.com>
2016-06-19 22:36:40 +0500

    19.06.2016 22:36

--- experiments/tcpsrv/functions.go
+++ experiments/tcpsrv/functions.go
@@ -8,8 +8,8 @@ import (
 
 func fillFunctions() map[string]interface{} {
 	functions := make(map[string]interface{})
-	functions["test"] = func(conn net.Conn, params []string) {
-		conn.Write([]byte(fmt.Sprintf("Test message with params! %s\n", strings.Join(params, "|"))))
+	functions["WAR"] = func(conn net.Conn, params []string) {
+		conn.Write([]byte(fmt.Sprintf("Test message with params: %s\n", strings.Join(params, "|"))))
 	}
 	return functions
 }
