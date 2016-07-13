a4ab0afc101ff7cbee5d7a4ebb9a0aa97ad27578
e4stw00d <e4stw00d@icloud.com>
2016-07-13 09:08:15 +0500

    13.07.2016 09:08

--- experiments/twitch.rb
+++ experiments/twitch.rb
@@ -1,19 +1,23 @@
 require 'socket'
 
-name = "nalcs1"
+abort("Not enough arguments") if ARGV.count == 0
+
+name = ARGV[0]
 s = TCPSocket.open "irc.chat.twitch.tv", 6667
 s.puts "PASS oauth:7mlkb36s7gtzyycdsfo45ydsr4ltqf"
 s.puts "NICK e4stw00d"
 s.puts "JOIN ##{name}"
 
-while line = s.gets
-	temp = line.split("PRIVMSG ##{name} :")
-	if temp.count > 1
-		nick = temp[0].split('!')[0][1..-1]
-		text = temp[1]
+begin
+	while line = s.gets
+		temp = line.split("PRIVMSG ##{name} :")
+		if temp.count > 1
+			nick = temp[0].split('!')[0][1..-1]
+			text = temp[1]
 
-		puts "#{nick} - #{text}"
+			puts "#{nick} - #{text}"
+		end
 	end
+rescue SystemExit, Interrupt
+	s.close
 end
-
-s.close
