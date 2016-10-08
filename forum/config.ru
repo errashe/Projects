17830ddde7d54da8a82c144e7bf6fd65948fcaf0
Александр Бутенко <e4stw00d@icloud.com>
2016-10-08 21:35:14 +0500

    08.10.2016 21:35

--- forum/config.ru
+++ forum/config.ru
@@ -1,5 +1,6 @@
 require "sinatra"
 require "sinatra/reloader" if development?
+require "sinatra/flash"
 
 require "sqlite3"
 require "require_all"
@@ -7,5 +8,7 @@ require "require_all"
 require_all "./src/*.rb"
 # require "./src/models.rb"
 # require "./src/main.rb"
+
+use Rack::MethodOverride
 
 run App
--- forum/src/main.rb
+++ forum/src/main.rb
@@ -1,13 +1,32 @@
 class App < Sinatra::Base
 	configure do
+		register Sinatra::Flash
 		register Sinatra::Reloader if development?
 
 		set :views, Proc.new { File.join(root, "..", "views") }
 		set :public_folder, Proc.new { File.join(root, "..", "public") }
+
+		set :session_secret, "SUPER SECRET STRING"
+		set :sessions, :enable
 	end
 
 	helpers do
 		def partial(name); erb name, :layout => false; end
+		def user; User.get(session[:user]); end
+	end
+
+	post "/login" do
+		if user = User.first(params)
+			session[:user] = user[:id]
+		else
+			flash[:error] = "Wrong auth data"
+		end
+		redirect to request.referrer
+	end
+
+	get "/logout" do
+		session[:user] = nil
+		redirect to request.referrer
 	end
 
 	get "/" do
@@ -15,20 +34,30 @@ class App < Sinatra::Base
 		erb :sections
 	end
 
-	get "/section/:id" do
-		@subsections = Subsection.all(:section_id => params[:id])
-		erb :subsections
+	get "/subsection/:id" do
+		@subsection = Subsection.get(params[:id])
+		erb :themes
 	end
 
-	get "/subsection/:id" do
-		@themes = Theme.all(:subsection_id => params[:id])
-		erb :themes
+	get "/subsection/:id/create_theme" do
+		erb :create_theme
+	end
+
+	put "/subsection/:id/create_theme" do
+		@subsection = Subsection.get(params[:id])
+		@subsection.themes.create(:user_id => user[:id], :name => params[:name], :text => params[:name])
+		redirect to "/subsection/#{params[:id]}"
 	end
 
 	get "/theme/:id" do
 		@theme = Theme.get(params[:id])
-		@messages = Message.all(:theme_id => params[:id])
 		erb :messages
+	end
+
+	put "/theme/:id" do
+		@theme = Theme.get(params[:id])
+		@theme.messages.create(:user_id => user[:id], :text => params[:text])
+		redirect to request.referrer
 	end
 
 	get "/tree" do
