ce15773d1feae6dd032f097958a55f6d1247c0d8
Александр Бутенко <e4stw00d@icloud.com>
2016-11-26 10:48:41 +0500

    26.11.2016 10:48

---
+++ captcha/main.sh
@@ -0,0 +1,9 @@
+convert img.jpg  -format %c -depth 8  histogram:info:histogram_image.txt
+String_color=$(sort -n histogram_image.txt | tail -1)
+[[ $String_color =~ (.[0-9A-zА-я]{6}) ]] && Code_color=${BASH_REMATCH[0]}
+rm -rf histogram_image.txt
+
+convert img.jpg -fill black -fuzz 80% +opaque $Code_color img2.jpg
+convert img2.jpg -fill white -fuzz 10% +opaque "#000000" img3.jpg
+
+# convert img3.jpg -blur 0x2 -level 40%,80% img4.jpg
