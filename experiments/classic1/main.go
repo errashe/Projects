3ad20d3badb564c20968163ee58e8bb98cd48bac
Александр Бутенко <e4stw00d@icloud.com>
2017-04-10 08:56:02 +0500

    10.04.2017 08:55

--- experiments/classic1/main.go
+++ experiments/classic1/main.go
@@ -2,15 +2,27 @@ package main
 
 import (
 	. "fmt"
-	// "net/http"
+	"net/http"
 	"os"
+	"sync"
 )
 
 var port string
+var wg sync.WaitGroup
 
-func main() {
+func init() {
 	if port = Sprintf(":%s", os.Getenv("PORT")); port == ":" {
 		port = ":8000"
 	}
-	Println(port)
+}
+
+func main() {
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		Println(http.ListenAndServe(port, nil))
+	}()
+	Printf("Server started at %s port\n", port)
+	wg.Wait()
 }
